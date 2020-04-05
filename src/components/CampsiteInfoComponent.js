828275b9e08ebb1fea7d13e3bcdd3850a2713c5c
jdedji <jeandedji2016@gmail.com>
2020-04-05 13:43:04 -0400

    redux

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,8 +1,13 @@
-import React, {/*  "Component" */} from 'react'; //Component is revoved because class compo no longuer exist
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
-import {CAMPSITES} from '../shared/campsites'
+import React, { Component } from 'react'; //Component is revoved because class compo no longuer exist
+import { Card, CardImg, CardText, CardBody,Modal,ModalHeader,ModalBody, Label, Col, CardTitle, Breadcrumb, Button,BreadcrumbItem, ModalFooter } from 'reactstrap';
+import {CAMPSITES} from '../shared/campsites';
+import {Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 
+const required = val => val && val.length;
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
+const validName= val => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 
 function RenderCampsite({campsite}) {
     return(
@@ -28,11 +33,12 @@ return(<div className ="col-md-5 m-1">
 <p>{comment.text} <br/>
 ,{comment.author},{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: 
 '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+
 </div>
     )
 
 })}
-
+<CommentForm/>
 </div> 
         ); 
         } 
@@ -71,7 +77,96 @@ function CampsiteInfo(props) {
     
     return <div />;
         }
+class CommentForm extends Component{
+    constructor(props){
+        super(props);
+         this.state = {
+             Modal: false,
+             className: "className"
+         }
 
+         this.toggleModal = this.toggleModal.bind(this);
+    } 
+
+         render(){
+             return(
+                 <div className = "container">
+                 <Button type = "outline" onClick= {this.toggleModal}>Submit Comment</Button>
+                 
+
+                 <Modal isOpen={this.state.Modal} toggle={this.toggleModal} className={this.state.className}>
+                 <ModalHeader toggle={this.toggleModal}>Modal title</ModalHeader>
+                 <ModalBody>
+                  <LocalForm onSubmit={values => this.handleSubmit(values)}> 
+                      <div className="form-group">
+                      <Col md= {(4)}>
+                          <Label htmlFor="rating">Rating</Label> <br/> 
+                          <Control.select model=".rating"  id="rating">
+                           <option>1</option>
+                           <option>2</option>
+                           <option>3</option>
+                           <option>4</option>
+                           <option>5</option>
+                       </Control.select>
+                       </Col>
+                       </div>
+                       
+                       <div className="form-group">
+                          
+                       <Label htmlFor="rating">Your name</Label> <br/> 
+                       <Control.text model=".author"  id="author" Name="author"placeholder="Your Name"
+                       
+                       validators={{
+                                            required,
+                                            validName, 
+                                            minLength: minLength(2),
+                                            maxLength: maxLength(15)
+                                        }}
+
+                        />
+                              <Errors
+                                        className="text-danger"
+                                        model=".author"
+                                        show="touched"
+                                        component="div"
+                                        messages={{
+                                            required: 'Required',
+                                            validName: 'Invalid Name',
+                                            minLength: 'Must be at least 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                      />
+                       </div>
+                       <div className="form-group">
+                       <Label htmlFor="rating">Comment</Label> <br/> 
+                       <Control.textarea model=".text"  id="text"></Control.textarea>
+                       </div>
+                      
+                       <Button color="primary" onClick={this.toggleModal}>Submit</Button>
+                     
+                   </LocalForm>
+               </ModalBody>
+             
+              </Modal>
+             
+             </div>
+             )
+         }
+    
+         toggleModal(){
+           this.setState(Modal)
+                
+           
+          function Modal(prev){
+              return (
+                {Modal: !prev.Modal}
+              )
+             
+          }
+           }
+         
+    
+}
 export default CampsiteInfo;
 
 
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import Directory from './/DirectoryComponent';
+import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from  './ContactComponent';
 import About from './AboutComponent';
-import Contact from './ContactComponent';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import { CAMPSITES } from '../shared/campsites'; //* shared link one . is addded to go down to find App.js the other faulder
-import { COMMENTS } from '../shared/comments';
-import { PARTNERS } from '../shared/partners';
-import { PROMOTIONS } from '../shared/promotions';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-class Main extends Component {
+//import { CAMPSITES } from '../shared/campsites'; //* shared link one . is addded to go down to find App.js the other faulder
+//import { COMMENTS } from '../shared/comments';
+//import { PARTNERS } from '../shared/partners';
+//import { PROMOTIONS } from '../shared/promotions';
+
+{/*lass Main extends Component {
     constructor(props) {
         super(props);
-        this.state = {
+        this.props = {
             campsites: CAMPSITES,
             comments: COMMENTS,
             partners: PARTNERS,
@@ -23,19 +25,28 @@ class Main extends Component {
            
         };
     }
+*/}
 
+const mapStateToProps = state => {
+    return {
+        campsites: state.campsites,
+        comments: state.comments,
+        partners: state.partners,
+        promotions: state.promotions
+    };
+};
     
 
-
+class Main extends Component {
     render() {
 
         const HomePage = () => {
             return (
                 <Home
                 
-                campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
-                promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
-                partner={this.state.partners.filter(partner => partner.featured)[0]}
+                campsite={this.props.campsites.filter(campsite => campsite.featured)[0]}
+                    promotion={this.props.promotions.filter(promotion => promotion.featured)[0]}
+                    partner={this.props.partners.filter(partner => partner.featured)[0]}
                 />
                 
                 
@@ -47,29 +58,28 @@ class Main extends Component {
  const CampsiteWithId = ({match}) => {
     return (
         <CampsiteInfo 
-            campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
-            comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+            campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+            comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
         />
     );
 };      
       return (
             <div>
-                <Header />
+                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
-                    <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
-                    <Route exact path='/contactus' component={Contact} />
-                    <Route exact path='/aboutus' component={()=><About partners={this.state.partners}/>} />
-
+                    <Route exact path='/directory' render={() => <Directory campsites={this.props.campsites} />} />
                     <Route path='/directory/:campsiteId' component={CampsiteWithId} />
+                    <Route exact path='/contactus' component={Contact} />
+                    <Route exact path='/aboutus' render={() => <About partners={this.props.partners} /> } />
                     <Redirect to='/home' />
-                </Switch> 
-                            
-                 <Footer />
+                </Switch>
+                <Footer />
           </div>
           
       );
   }
 }
 
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main));
+ //allow to take state from the Redux
